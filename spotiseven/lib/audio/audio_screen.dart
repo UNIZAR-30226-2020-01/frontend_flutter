918a934ab91c843c7963ff5597acb373808a5445
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-03-14 23:42:34 +0100

    Primera aproximacion con stack

--- spotiseven/lib/audio/audio_screen.dart
+++ spotiseven/lib/audio/audio_screen.dart
@@ -34,117 +34,139 @@ class _PlayingScreenState extends State<PlayingScreen> {
   Widget build(BuildContext context) {
     final double border_radius = 45;
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        title: Text(
-          // TODO: Poner esto en el cuerpo del Scaffold para aumentar el tamaño y poner una imagen de fondo.
-          // TODO: Change this dinamically
-          'MOTIVATION',
-          style: TextStyle(
-            // TODO: Poner la fuentes
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 3,
-          ),
-        ),
-      ),
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
-//        color: Colors.green[500],
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(border_radius),
-              topRight: Radius.circular(border_radius)),
-          border: Border.all(color: Colors.green),
-          color: Colors.green[500],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                IconButton(
-                  onPressed: () => print('share'),
-                  icon: Icon(Icons.share),
-                  iconSize: 40,
-                ),
-              ],
-            ),
-            CircleAvatar(
-              // TODO: Change this dinamically
-              backgroundImage: NetworkImage(
-                  'https://yt3.ggpht.com/a/AATXAJzgtF2V2m4KsP1ZHU12UcqzoDBEL4GH4e_CmQ=s288-c-k-c0xffffffff-no-rj-mo'),
-              radius: 90,
-            ),
-            Text(
-              // TODO: Change this dinamically
-              'The Song Name',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
-            ),
-            Text(
-              // TODO: Change this dinamically
-              'Artist Name',
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-            // Controles de musica
-            buildAudioControlls(),
-            // ProgressBar -> En segundos
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FutureBuilder(
-                  future: _audioPlayer.getDuration(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Slider(
-                        // TODO: Change this colors and style
-                        activeColor: Colors.black,
-                        inactiveColor: Colors.green[800],
-                        min: 0,
-                        max: (snapshot.data as Duration).inSeconds.toDouble(),
-                        value: _time.toDouble(),
-                        // TODO: igual esto se puede hacer de otra forma
-                        onChanged: (value) {
-                          print('${value.toInt().toString()}');
-                          _audioPlayer
-                              .seekPosition(Duration(seconds: value.toInt()));
-                          setState(() {
-                            _time = value.toInt();
-                          });
-                        },
-                      );
-                    } else {
-                      return Slider(
-                        min: 0,
-                        max: 0,
-                        value: 0,
-                      );
-                    }
-                  },
-                ),
-                // Tiempo en segundos
-                Text(
-                  '${Duration(seconds: _time).toString().substring(2, 7)}',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              width: MediaQuery.of(context).size.width,
+              height: 80,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://image.shutterstock.com/image-photo/serious-computer-hacker-dark-clothing-600w-1557297230.jpg'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ],
+                child: Center(
+                  child: Text(
+                    // TODO: Poner esto en el cuerpo del Scaffold para aumentar el tamaño y poner una imagen de fondo.
+                    // TODO: Change this dinamically
+                    'MOTIVATION',
+                    style: TextStyle(
+                      // TODO: Poner la fuentes
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 3,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            // Controles de playlist
-            buildPlaylistControlls(),
+            Positioned(
+              top: 80,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(border_radius),
+                      topRight: Radius.circular(border_radius)),
+                  color: Color(0xff9ad1e5),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () => print('share'),
+                          icon: Icon(Icons.share),
+                          iconSize: 40,
+                        ),
+                      ],
+                    ),
+                    CircleAvatar(
+                      // TODO: Change this dinamically
+                      backgroundImage: NetworkImage(
+                          'https://yt3.ggpht.com/a/AATXAJzgtF2V2m4KsP1ZHU12UcqzoDBEL4GH4e_CmQ=s288-c-k-c0xffffffff-no-rj-mo'),
+                      radius: 90,
+                    ),
+                    Text(
+                      // TODO: Change this dinamically
+                      'The Song Name',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      // TODO: Change this dinamically
+                      'Artist Name',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    // Controles de musica
+                    buildAudioControlls(),
+                    // ProgressBar -> En segundos
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        FutureBuilder(
+                          future: _audioPlayer.getDuration(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Slider(
+                                // TODO: Change this colors and style
+                                activeColor: Colors.black,
+                                inactiveColor: Colors.green[800],
+                                min: 0,
+                                max: (snapshot.data as Duration)
+                                    .inSeconds
+                                    .toDouble(),
+                                value: _time.toDouble(),
+                                // TODO: igual esto se puede hacer de otra forma
+                                onChanged: (value) {
+                                  print('${value.toInt().toString()}');
+                                  _audioPlayer.seekPosition(
+                                      Duration(seconds: value.toInt()));
+                                  setState(() {
+                                    _time = value.toInt();
+                                  });
+                                },
+                              );
+                            } else {
+                              return Slider(
+                                min: 0,
+                                max: 0,
+                                value: 0,
+                              );
+                            }
+                          },
+                        ),
+                        // Tiempo en segundos
+                        Text(
+                          '${Duration(seconds: _time).toString().substring(2, 7)}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Controles de playlist
+                    buildPlaylistControlls(),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
