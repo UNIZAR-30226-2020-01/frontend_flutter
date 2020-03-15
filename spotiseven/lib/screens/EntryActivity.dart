ff6489a26927571af8461134e9ab6065cb459aa7
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-03-15 13:21:13 +0100

    Boton y barra de reproducción conmutantes

--- spotiseven/lib/screens/EntryActivity.dart
+++ spotiseven/lib/screens/EntryActivity.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../GenericLoginButton.dart';
+import '../generic_components/GenericLoginButton.dart';
 
 class EntryActivity extends StatelessWidget {
   @override
--- spotiseven/lib/screens/home/main_screen.dart
+++ spotiseven/lib/screens/home/main_screen.dart
@@ -7,7 +7,6 @@ class MainScreenWrapper extends StatefulWidget {
 }
 
 class _MainScreenWrapperState extends State<MainScreenWrapper> {
-
   // Select Screen from BottomNavigationBar
   int _currentIndex = 0;
   // Children to wrap
@@ -17,7 +16,8 @@ class _MainScreenWrapperState extends State<MainScreenWrapper> {
     HomeScreenWrapper(),
     HomeScreenWrapper(),
   ];
-
+  // To show reproductor bar
+  bool _showReprBar = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +44,89 @@ class _MainScreenWrapperState extends State<MainScreenWrapper> {
           });
         },
       ),
-      body: _children[_currentIndex],
+      floatingActionButton: !_showReprBar
+          ? FloatingActionButton(
+              onPressed: () {
+                print('pressed fab');
+                setState(() {
+                  _showReprBar = true;
+                });
+              },
+              // TODO: Change this color
+              backgroundColor: Colors.black,
+              // TODO: Change this icon
+              child: Icon(Icons.airplanemode_active),
+            )
+          : null,
+      body: Stack(
+        children: <Widget>[
+          _children[_currentIndex],
+          _showReprBar
+              ? Positioned(
+                  bottom: 0,
+                  width: MediaQuery.of(context).size.width,
+                  child: buildReproBar(),
+                )
+              : SizedBox(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildReproBar() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 15, 10, 10),
+      height: 80,
+      // TODO: Change this color
+      color: Colors.black,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            // TODO: Change this dinamically
+            backgroundImage: NetworkImage(
+                'https://yt3.ggpht.com/a/AATXAJzgtF2V2m4KsP1ZHU12UcqzoDBEL4GH4e_CmQ=s288-c-k-c0xffffffff-no-rj-mo'),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // TODO: Change this dinamically
+              Text(
+                'Song name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+//              SizedBox(height: 10),
+              Text(
+                'Artist Name',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: () => print('play'),
+            icon: Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+            ),
+            iconSize: 40,
+          ),
+          IconButton(
+            onPressed: () => setState(() => _showReprBar = false),
+            icon: Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.white,
+            ),
+            iconSize: 40,
+          ),
+        ],
+      ),
     );
   }
 }
