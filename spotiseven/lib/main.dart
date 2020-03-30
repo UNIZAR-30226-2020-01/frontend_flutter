a17a8a90c273cc19c1a01afd7b868f3a0078b6f5
hayk99 <haykk1999@gmail.com>
2020-03-30 11:14:10 +0200

    changed themedata colors

--- spotiseven/lib/main.dart
+++ spotiseven/lib/main.dart
@@ -8,11 +8,43 @@ import 'package:spotiseven/screens/audio/audio_screen.dart';
 import 'package:spotiseven/screens/loginScreen/login.dart';
 import 'package:spotiseven/screens/splashScreen/splash_screen.dart';
 
+const MaterialColor white = const MaterialColor(
+  0xFFFFFFFF,
+  const <int, Color>{
+    50: const Color(0xFFFFFFFF),
+    100: const Color(0xFFFFFFFF),
+    200: const Color(0xFFFFFFFF),
+    300: const Color(0xFFFFFFFF),
+    400: const Color(0xFFFFFFFF),
+    500: const Color(0xFFFFFFFF),
+    600: const Color(0xFFFFFFFF),
+    700: const Color(0xFFFFFFFF),
+    800: const Color(0xFFFFFFFF),
+    900: const Color(0xFFFFFFFF),
+  },
+);
+
+const MaterialColor black = const MaterialColor(
+  0xFFFFFFFF,
+  const <int, Color>{
+    50: const Color(0x00000000),
+    100: const Color(0x00000000),
+    200: const Color(0x00000000),
+    300: const Color(0x00000000),
+    400: const Color(0x00000000),
+    500: const Color(0x00000000),
+    600: const Color(0x00000000),
+    700: const Color(0x00000000),
+    800: const Color(0x00000000),
+    900: const Color(0x00000000),
+  },
+);
+
 void main() => runApp(MaterialApp(
         title: 'Proyecto SW',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
-         // accentColor: Colors.black,
+          primarySwatch: white,
+          accentColor: Colors.yellow[300],
         ),
         debugShowCheckedModeBanner: false,
         //initialRoute: '/login',
