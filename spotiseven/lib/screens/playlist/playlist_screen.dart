e42aaebb84865225266970268635dcd04c61b8d0
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-04-01 17:28:39 +0200

    Primera version de la nueva pantalla de playlist

--- spotiseven/lib/screens/playlist/playlist_screen.dart
+++ spotiseven/lib/screens/playlist/playlist_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spotiseven/audio/playingSingleton.dart';
 // Clase Playlist
@@ -9,184 +10,245 @@ import 'package:spotiseven/audio/utils/song.dart';
 // Fuentes de Google
 import 'package:google_fonts/google_fonts.dart';
 
-class PlaylistScreen extends StatelessWidget {
+class PlaylistScreen extends StatefulWidget {
   final Playlist playlist;
 
   PlaylistScreen({this.playlist});
 
   @override
+  _PlaylistScreenState createState() => _PlaylistScreenState();
+}
+
+class _PlaylistScreenState extends State<PlaylistScreen> {
+  // Control del scroll
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()..addListener(() => setState(() {}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final double border_radius = 45;
-    // Version alternativa dinamica
-    return Container(
-      color: Color(0xff9ad1e5),
-      child: RefreshIndicator(
-        onRefresh: () => Future.delayed(
-            Duration(microseconds: 1), () => print('recargando')),
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: <Widget>[
-            SliverAppBar(
-              backgroundColor: Colors.cyan,
-              elevation: 0,
-              floating: true,
-              snap: false,
-              pinned: false,
-              // Efectos
-              stretch: true,
-              onStretchTrigger: () => Future.delayed(
-                  Duration(microseconds: 1), () => print('stretch')),
-              expandedHeight: 300.0,
-              flexibleSpace: FlexibleSpaceBar(
-                // Efectos
-                stretchModes: [
-                  StretchMode.zoomBackground,
-                  StretchMode.blurBackground,
-                  StretchMode.fadeTitle,
-                ],
-                title: Text(
-                  '${playlist.title}',
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    fontSize: 25,
-                    letterSpacing: 3,
-                    wordSpacing: 3,
-                  ),
-                ),
-                centerTitle: true,
-                background: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    Image.network(
-                      '${playlist.photoUrl}',
-                      fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            child: RefreshIndicator(
+              onRefresh: () => Future.delayed(
+                  Duration(microseconds: 1), () => print('recargando')),
+              child: CustomScrollView(
+                controller: _scrollController,
+                physics: const BouncingScrollPhysics(),
+                slivers: <Widget>[
+                  SliverAppBar(
+                    backgroundColor: Colors.black,
+                    leading: IconButton(
+                      onPressed: () => print('Pressed 3 dot button'),
+                      icon: Icon(Icons.more_vert),
+                      color: Colors.white,
                     ),
-                    const DecoratedBox(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment(0.0, 0.5),
-                          end: Alignment(0.0, 0.0),
-                          colors: <Color>[
-                            Color(0x60000000),
-                            Color(0x00000000),
+                    elevation: 0,
+                    // Propiedades sliverappbar
+                    floating: false,
+                    snap: false,
+                    pinned: true,
+                    // Efectos
+                    stretch: true,
+                    onStretchTrigger: () => Future.delayed(
+                        Duration(microseconds: 1), () => print('stretch')),
+                    expandedHeight: 300,
+                    flexibleSpace: FlexibleSpaceBar(
+                      // Efectos
+                      stretchModes: [
+                        StretchMode.zoomBackground,
+                        StretchMode.blurBackground,
+                        StretchMode.fadeTitle,
+                      ],
+                      title: SizedBox(
+                        height: 200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              '${widget.playlist.title}',
+                              style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                                fontSize: 25,
+                                letterSpacing: 3,
+                                wordSpacing: 3,
+                              ),
+                              textAlign: TextAlign.end,
+                            ),
                           ],
                         ),
                       ),
+                      centerTitle: true,
+                      background: Image.network(
+                        '${widget.playlist.photoUrl}',
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 100),
-                  // TODO: Aplicar estilo
-                  child: RaisedButton(
-                    onPressed: () => PlayingSingleton()
-                      ..setPlayList(playlist)
-                      ..randomize()
-                      ..play(PlayingSingleton().song),
-                    child: Text('PLAY RANDOM'),
                   ),
-                )
-              ]),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return buildSongPreview(playlist.playlist[index], playlist);
-                },
-                childCount: playlist.playlist.length,
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      // TODO: Esto es para hacer pruebas con listas largas
+                      (BuildContext context, int index) {
+                        return buildSongPreview_v2(
+                            widget.playlist.playlist[
+                                index],
+                            widget.playlist,
+                            context);
+                      },
+                      childCount: widget.playlist.playlist.length,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          _fabReproduction(),
+        ],
       ),
     );
   }
 
-  Widget buildSongPreview(Song s, Playlist p) {
-    return FlatButton(
-      onPressed: () async {
-        var playingSingleton = PlayingSingleton();
-        playingSingleton.setPlayList(p);
-        await playingSingleton.play(s);
-        print('Reproduciendo ${playingSingleton.song.title}');
-      },
-      child: Container(
-        margin: EdgeInsets.fromLTRB(15, 15, 0, 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage: NetworkImage(s.photoUrl),
-              radius: 40,
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('${s.title}'),
-                Text('${s.album.artista}'),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  Widget _fabReproduction() {
+    //starting fab position
+    final double defaultTopMargin = 270 - 4.0;
+    //pixels from top where scaling should start
+    final double scaleStart = 96.0;
+    //pixels from top where scaling should end
+    final double scaleEnd = scaleStart / 2;
 
-  Widget buildSongPreviewTambo(Song s, Playlist p) {
-    return Column(
-      children: <Widget>[
-        FlatButton(
-          onPressed: () async {
-            var playingSingleton = PlayingSingleton();
-            playingSingleton.setPlayList(p);
-            await playingSingleton.play(s);
-            print('Reproduciendo ${playingSingleton.song.title}');
-          },
-          child: Container(
-            margin: EdgeInsets.fromLTRB(15, 15, 0, 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 80,
-                  width: 80,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(s.photoUrl),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('${s.title}'),
-                    Text('${s.album.artista}'),
-                  ],
-                ),
-              ],
+    double top = defaultTopMargin;
+    double scale = 1.0;
+
+    if (_scrollController.hasClients) {
+      // Modificacion de la altura del boton
+      double offset = _scrollController.offset;
+      top -= offset;
+      if (offset < defaultTopMargin - scaleStart) {
+        //offset small => don't scale down
+        scale = 1.0;
+      } else if (offset < defaultTopMargin - scaleEnd) {
+        //offset between scaleStart and scaleEnd => scale down
+        scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
+      } else {
+        //offset passed scaleEnd => hide fab
+        scale = 0.0;
+      }
+    }
+    return Positioned(
+      top: top,
+      right: 16,
+      child: Transform(
+        transform: new Matrix4.identity()..scale(scale),
+        child: Container(
+          child: RaisedButton(
+            onPressed: () => PlayingSingleton()
+              ..setPlayList(widget.playlist)
+              ..randomize()
+              ..play(PlayingSingleton().song),
+            child: Text('PLAY', style: GoogleFonts.roboto(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.normal,
+            )),
+            elevation: 0,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))
             ),
           ),
         ),
-        Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Divider(
-              color: Colors.grey[750],
-              thickness: 1.5,
-            )),
-      ],
+      ),
+    );
+  }
+
+  Widget buildSongPreview_v2(Song s, Playlist p, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 15, 30, 15),
+      child: GestureDetector(
+        onTap:() async {
+          var playingSingleton = PlayingSingleton();
+          playingSingleton.setPlayList(p);
+          await playingSingleton.play(s);
+          print('Reproduciendo ${playingSingleton.song.title}');
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.network(
+                    s.photoUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(
+                  width: 1,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width * 0.7,
+              padding: EdgeInsets.only(left: 30, right: 10),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '${s.title}',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '${s.album.artista}',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.more_vert),
+                    color: Colors.white,
+                    onPressed: () => print('Presed ${s.title} options'),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
