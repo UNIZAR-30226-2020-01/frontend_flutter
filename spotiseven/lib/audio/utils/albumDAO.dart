cd0eabb32f1c081d7c61297a2271eca2726f106e
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-04-05 13:38:18 +0200

    Integracion del artista con la API REST

--- spotiseven/lib/audio/utils/albumDAO.dart
+++ spotiseven/lib/audio/utils/albumDAO.dart
@@ -12,12 +12,24 @@ class AlbumDAO {
   static final String _url = 'https://s7-rest.francecentral.cloudapp.azure.com';
 
   static Future<List<Album>> getAllAlbums() async {
-    List<dynamic> response = await _client.get('$_url/albums').then((Response resp) => jsonDecode(resp.body));
+    List<dynamic> response = await _client.get('$_url/albums').then((Response resp) {
+      if(resp.statusCode == 200){
+        return jsonDecode(resp.body);
+      }else{
+        throw Exception('No tienes permisos para acceder a este recurso');
+      }
+    });
 //    print(response);
     return response.map((d) => Album.fromJSON(d)).toList();
   }
 
   static Future<Album> getByURL(String url) async {
-    return await _client.get('$url').then((Response response) => Album.fromJSON(jsonDecode(response.body)));
+    return await _client.get('$url').then((Response response) {
+      if(response.statusCode == 200){
+        return Album.fromJSON(jsonDecode(response.body));
+      }else{
+        throw Exception('No tienes permisos para acceder a este recurso');
+      }
+    });
   }
 }
---
+++ spotiseven/lib/audio/utils/artistDAO.dart
@@ -0,0 +1,34 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+// Class Artist
+import 'package:spotiseven/audio/utils/artist.dart';
+
+class ArtistDAO {
+  static final Client _client = Client();
+  static final String _url = 'https://s7-rest.francecentral.cloudapp.azure.com';
+
+  static Future<List<Artist>> getAllArtist() async {
+    List<Artist> list = await _client.get('$_url/artists/').then((Response response){
+      if(response.statusCode == 200){
+        // TODO: Cambiar esto con la version final (comprobar el fromJSON)
+        return (jsonDecode(response.body) as List).map((d) => Artist.fromJSON(d)).toList();
+      }else{
+        throw Exception('No tienes permiso para acceder a este recurso');
+      }
+    });
+    return list;
+  }
+
+  // Peticion a la URL del recurso del artista
+  static Future<Artist> getByURL(String url) async {
+    return await _client.get('$url').then((Response response) {
+      if(response.statusCode == 200){
+        return Artist.fromJSON(jsonDecode(response.body));
+      }else{
+        throw Exception('No tienes permisos para acceder a este recurso');
+      }
+    });
+  }
+}
